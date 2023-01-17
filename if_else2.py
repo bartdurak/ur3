8a930ac4abe7cab10a77dd9757e7f3d65a670ffa
bdurak <projektator@gmail.com>
2023-01-17 03:08:40 +0100

    Udemy py

---
+++ if_else2.py
@@ -0,0 +1,10 @@
+#"a = 4
+#b = 3
+a=int(input())
+b=int(input())
+if (a > b):
+    print("a jest większe od b")
+elif (a < b):
+    print(" b jest większe od a")
+else:
+    print("a jest równe b: ")
