8f6e4a388a4136b4f9335b9e7762f5d68793f5fe
bdurak <projektator@gmail.com>
2023-01-17 03:08:40 +0100

    Udemy py

---
+++ dodawaie_py.py
@@ -0,0 +1,17 @@
+" przykład if else pythona"
+wybor = input("  + dodawanie , - odejmowanie, * mnożenie, / dzielenie ")
+
+a = int(input())
+b = int(input())
+
+if (wybor == '+'):
+    print(a + b)
+elif (wybor == '-'):
+    print(a-b)
+elif (wybor == '*'):
+    print( a*b)
+elif (wybor == '/'):
+    print( a/b)
+else:
+    print("a jest równe b: ")
+
