ffbc7d24d28873801bd352a397b6ebafc76d3c5e
bartdurak <projektator@gmail.com>
2023-01-19 22:30:44 +0100

    Udemy py

---
+++ if_bulki.py
@@ -0,0 +1,8 @@
+wartosc = int(input('Podaj wartość: '))
+if (wartosc > 1 and wartosc  < 10):
+    print("Wartość jest od 1 do 10")
+
+a = 5
+b = 2
+if (a==5 or b==3):
+      print("Wartość jest prawidłowa")
