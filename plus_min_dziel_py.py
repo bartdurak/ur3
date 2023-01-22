0f83564e1670acff5fafbfc84a48019184f6f39f
bartdurak <projektator@gmail.com>
2023-01-22 17:15:37 +0100

    Udemy dla 1a

--- plus_min_dziel_py.py
+++ plus_min_dziel_py.py
@@ -3,15 +3,12 @@ wybor = input("  + dodawanie , - odejmowanie, * mnożenie, / dzielenie ")
 
 a = int(input())
 b = int(input())
-
 if (wybor == '+'):
     print(a + b)
 elif (wybor == '-'):
-    print(a-b)
+    print(a - b)
 elif (wybor == '*'):
-    print( a*b)
-elif (wybor == '/'):
-    print( a/b)
+    print(a * b)
+elif (wybor == '/'):print(a / b)
 else:
     print("a jest równe b: ")
-
---
+++ while_4.py
@@ -0,0 +1,16 @@
+wynik = 0
+i = 0
+# przykład pętli python while
+"""while i< 4:
+    x = int(input("Podaj liczbę: "))
+    wynik += x
+    i += 1
+    print ("wynik dodawania to: ", wynik)
+"""
+# przykład pętli python for
+
+#for i in range (0,4):
+for i in "gitara":
+    x = int(input("Podaj liczbę: "))
+    wynik += x
+    print("wynik dodawania to: ", wynik)
